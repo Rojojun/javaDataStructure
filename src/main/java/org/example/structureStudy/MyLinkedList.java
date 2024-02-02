b17a5cab04ba44510ab69e03757d6e7cebe7b304
rojojun <zoloman316@gmail.com>
2024-02-02 20:45:27 +0900

    3-5 실습 :: MyLinkedList 관련 클래스 생성

---
+++ src/main/java/org/example/structureStudy/MyLinkedList.java
@@ -0,0 +1,4 @@
+package org.example.structureStudy;
+
+public class MyLinkedList {
+}
---
+++ src/test/java/org/example/structureStudy/MyLinkedListTest.java
@@ -0,0 +1,7 @@
+package org.example.structureStudy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyLinkedListTest {
+
+}
