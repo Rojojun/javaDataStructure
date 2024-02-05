a6189d91db85d67d37290fec21e7e11b5c7e19de
rojojun <zoloman316@gmail.com>
2024-02-05 20:18:16 +0900

    3-5 실습 :: Test 코드 오류 수정

--- src/test/java/org/example/structureStudy/MyLinkedListTest.java
+++ src/test/java/org/example/structureStudy/MyLinkedListTest.java
@@ -1,8 +1,8 @@
 package org.example.structureStudy;
 
-import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeAll;
 
-import org.junit.Before;
+import java.util.ArrayList;
 
 
 /**
@@ -14,14 +14,14 @@ public class MyLinkedListTest extends MyArrayListTest {
     /**
      * @throws java.lang.Exception
      */
-    @Before
-    public void setUp() throws Exception {
-        list = new ArrayList<Integer>();
+    @BeforeAll
+    public static void setUp() throws Exception {
+        list = new ArrayList<>();
         list.add(1);
         list.add(2);
         list.add(3);
 
-        mylist = new MyLinkedList<Integer>();
+        mylist = new MyLinkedList<>();
         mylist.addAll(list);
     }
 }
